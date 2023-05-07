85707e4f563d148351a38ad2729b601781a33e41
Aleksandar Ivanov <smichess@gmail.com>
2023-05-07 16:49:04 +0300

    add many more comments

--- master/Alarm-Master/Alarm-Master/main.c
+++ master/Alarm-Master/Alarm-Master/main.c
@@ -20,6 +20,7 @@ uint32_t timeout = 25000;
 uint8_t timeoutPlayed = 0;
 char *password = "D4321";
 
+// Initialization for millis, uart and chipTunes libraries
 void init(){
     init_millis(F_CPU); //frequency the atmega328p is running at
     uart_init();
@@ -30,6 +31,7 @@ void init(){
     chipTunes_Init();
 }
 
+// Method to validate a protocol message. Checks for correct length and delimiters.
 int validateProtocol(char *msg){
     if(msg[0] == '$' && msg[2] == '#' && strlen(msg) == 8){
         return 1;
@@ -37,36 +39,33 @@ int validateProtocol(char *msg){
     return 0;
 }
 
-void handlePinChange(char *newEntry, char *lastEntry)
-{
-
-}
-
 int main(void) {
     init();
     
-    _delay_ms(2500);
+    _delay_ms(2500); // just a delay to give me time to reconnect serial console
     printf("Master init complete \n");
     fflush(stdout);
     
-    char *inputString = malloc(sizeof(char)*9);
-    char *lastDigits = "-----";
-    char *lastMotion = '1';
+    char *inputString = malloc(sizeof(char)*9); // string holding last input string
+    char *lastDigits = "-----"; // string holding the last keypad input
+    char *lastMotion = '1'; // holding the last state of the motion sensor
     
     while(1) {
-        uart_getstring(inputString,9);
+        uart_getstring(inputString,9); // periodic reading for input from Alarm-Slave
         
-        if(validateProtocol(inputString)){ // new input from slave
+        if(validateProtocol(inputString)){ // new input from slave, consider only if it's valid
+            // parsing the digits
             char *digits = malloc(sizeof(char) * 6);
             strncpy(digits, &inputString[3], 6);
-            printf("Read: %s\n",digits);
-            fflush(stdout);
+            //printf("Read: %s\n",digits);
+            //fflush(stdout);
             int skipTimeout = 0;
             
+            // State machine
             switch(controllerState)
             {
                 case DISARMED:
-                if(inputString[3] == 'A'){
+                if(inputString[3] == 'A'){ // 'A' is the arming key
                     timeoutPlayed = 1;
                     skipTimeout = 1;
                     controllerState = ARMED;
@@ -75,18 +74,18 @@ int main(void) {
                 }
                 break;
                 case ARMED:
-                if(strcmp(digits, password) == 0 && (lastEntry + timeout) > millis()){
+                if(strcmp(digits, password) == 0 && (lastEntry + timeout) > millis()){ // Disarm if password matches and no input timeout
                     controllerState = DISARMED;
                     skipTimeout = 1;
                     printf("Alarm disarmed.\n");
                     fflush(stdout);
                 }
-                if(inputString[1] == '0'){
+                if(inputString[1] == '0'){ // if the motion sensor detects movement, switch to alarming
                     controllerState = ALARMING;
                 }
                 break;
                 case ALARMING:
-                if(strcmp(digits, password) == 0){
+                if(strcmp(digits, password) == 0){ // password matches -> disarm
                     controllerState = DISARMED;
                     printf("Alarm disarmed.\n");
                     fflush(stdout);
@@ -94,21 +93,20 @@ int main(void) {
                 break;
             }
             
-            if(strcmp(digits, lastDigits) != 0 && skipTimeout == 0){
+            if(strcmp(digits, lastDigits) != 0 && skipTimeout == 0){ // tracking last pin input time
                 lastEntry = millis();
                 timeoutPlayed = 0;
             }
         }
         
         if((lastEntry + timeout) < millis() && !timeoutPlayed){
-            //timeout for pin entry
+            // timeout for pin entry, play a note
             chipTunes_Tone(NOTE_A5, 500);
             timeoutPlayed = 1;
             printf("Pin entry timeout.\n");
             fflush(stdout);
             } else if(controllerState == ALARMING){
-            printf("ALARMING.\n");
-            fflush(stdout);
+            // we're in alarming state, play alarm continuously
             chipTunes_Tone(NOTE_A2, 500);
         }
     }
